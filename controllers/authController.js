6748ecb249d6ec6eddaa99fbee780c4b85d1b6ef
lukasilverio94 <lukasilverio94@gmail.com>
2023-12-04 21:47:20 +0100

    handle error created

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,15 +2,19 @@ const User = require("../models/User");
 
 //Handle Errors
 const handleErrors = (err) => {
-  let error = {
+  let errors = {
     email: "",
     password: "",
   };
 
   //validation errors
   if (err.message.includes("user validation failed")) {
-    console.log(Object.values(err.errors));
+    Object.values(err.errors).forEach(({ properties }) => {
+      errors[properties.path] = properties.message;
+    });
   }
+
+  return errors;
 };
 
 // controller actions
@@ -29,8 +33,8 @@ module.exports.signup_post = async (req, res) => {
     const user = await User.create({ email, password });
     res.status(201).json(user);
   } catch (err) {
-    handleErrors(err);
-    res.status(400).send("error, user not created");
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
   }
 };
 
